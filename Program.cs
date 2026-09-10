ba98df60043ccf0527018d064eb5aaf60e269c1a
Jaydeep Gawade <jaydeepgawade42@gmail.com>
2026-09-10 11:34:07 +0530

    Commit

--- Program.cs
+++ Program.cs
@@ -21,11 +21,11 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // 2. Configure the HTTP request pipeline
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 app.UseCors("FlutterCors");
